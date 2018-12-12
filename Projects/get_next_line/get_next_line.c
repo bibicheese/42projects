0cbfd9b2a7727aaffb79d163a02c27efe572e8c8
Jeremie MONDINO <jmondino@e2r3p16.42.fr>
2018-12-12 18:50:13 +0100

    gnl

--- Projects/get_next_line/get_next_line.c
+++ Projects/get_next_line/get_next_line.c
@@ -6,13 +6,40 @@
 /*   By: jmondino <jmondino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 19:02:50 by jmondino          #+#    #+#             */
-/*   Updated: 2018/12/05 19:31:23 by jmondino         ###   ########.fr       */
+/*   Updated: 2018/12/12 18:48:04 by jmondino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "libft/libft.h"
+#include "get_next_line.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
 int		get_next_line(const int fd, char **line)
 {
-	
+	char	buff[BUFF_SIZE + 1];
+	char	*temp;
+	int		ret;
+	int		i;
+
+	i = 0;
+	while ((ret = read(fd, buff, BUFF_SIZE)))
+		buff[ret] = '\0';
+	while(buff[i] != '\n')
+		i++;
+	printf("temp = %s\n", buff);
+	printf("sub = %s\n", ft_strsub(buff, 0, i));
+	return (1);
+}
+
+int		main(int argc, char **argv)
+{
+	char *line;
+	int fd = open(argv[1], O_RDONLY);
+
+	get_next_line(fd, &line);
+	return 0;
 }
--- Projects/get_next_line/get_next_line.h
+++ Projects/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jmondino <jmondino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 19:10:33 by jmondino          #+#    #+#             */
-/*   Updated: 2018/12/05 23:53:30 by jmondino         ###   ########.fr       */
+/*   Updated: 2018/12/12 18:48:29 by jmondino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,5 @@
 # define BUFF_SIZE 32
 
 int		get_next_line(const int fd, char **line);
-{
-	
-}
 
 #endif
