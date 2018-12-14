788030e1c125c6d81b604310a024bedcb8ee2212
bibicheese <m.ondino@hotmail.fr>
2018-12-14 16:47:54 +0100

    je vois la 1er ligne mais pas les autres :<

--- Projects/get_next_line/get_next_line.c
+++ Projects/get_next_line/get_next_line.c
@@ -6,15 +6,15 @@
 /*   By: jmondino <jmondino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 19:02:50 by jmondino          #+#    #+#             */
-/*   Updated: 2018/12/12 18:48:04 by jmondino         ###   ########.fr       */
+/*   Updated: 2018/12/14 16:45:48 by jmondino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 
@@ -23,16 +23,17 @@ int		get_next_line(const int fd, char **line)
 	char	buff[BUFF_SIZE + 1];
 	char	*temp;
 	int		ret;
-	int		i;
 
-	i = 0;
+	temp = (char *)malloc(sizeof(char) * BUFF_SIZE + 1);
 	while ((ret = read(fd, buff, BUFF_SIZE)))
-		buff[ret] = '\0';
-	while(buff[i] != '\n')
-		i++;
-	printf("temp = %s\n", buff);
-	printf("sub = %s\n", ft_strsub(buff, 0, i));
-	return (1);
+	{
+	   	buff[ret] = '\0';
+		temp = ft_strjoin(temp, buff);
+	}
+	ret = 0;
+	while (temp[ret] != '\n')
+	   	ret++;
+   	*line = ft_strsub(temp, 0, ret);
 }
 
 int		main(int argc, char **argv)
@@ -41,5 +42,7 @@ int		main(int argc, char **argv)
 	int fd = open(argv[1], O_RDONLY);
 
 	get_next_line(fd, &line);
+		printf("%s", line);
 	return 0;
 }
+
--- Projects/get_next_line/get_next_line.h
+++ Projects/get_next_line/get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: jmondino <jmondino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 19:10:33 by jmondino          #+#    #+#             */
-/*   Updated: 2018/12/12 18:48:29 by jmondino         ###   ########.fr       */
+/*   Updated: 2018/12/14 16:39:39 by jmondino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 1
 
 int		get_next_line(const int fd, char **line);
 
