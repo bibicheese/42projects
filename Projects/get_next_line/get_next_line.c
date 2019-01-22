8bb7bc36f3fa0264e000776ec5c16cf12603bc22
Jeremie MONDINO <jmondino@e3r9p18.42.fr>
2019-01-22 15:49:47 +0100

    ...

--- Projects/get_next_line/get_next_line.c
+++ Projects/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jmondino <jmondino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 19:02:50 by jmondino          #+#    #+#             */
-/*   Updated: 2019/01/22 15:21:16 by jmondino         ###   ########.fr       */
+/*   Updated: 2019/01/22 15:48:39 by jmondino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ static int		ft_checkstock(char *buff, char **line, char **stock)
 {
 	char	*tmp;
 
-   	*line = ft_strsub(*stock, 0, ft_strchr(*stock, '\n') - *stock);
-   	tmp = (buff == NULL) ? ft_strdup(ft_strchr(*stock, '\n') + 1) :
-   	   ft_strjoin(ft_strchr(*stock, '\n') + 1, buff);
-  	ft_memdel((void **)stock);
-   	*stock = tmp;
-   	return (0);
+	*line = ft_strsub(*stock, 0, ft_strchr(*stock, '\n') - *stock);
+	tmp = (buff == NULL) ? ft_strdup(ft_strchr(*stock, '\n') + 1) :
+		ft_strjoin(ft_strchr(*stock, '\n') + 1, buff);
+	ft_memdel((void **)stock);
+	*stock = tmp;
+	return (0);
 }
 
 static int		ft_checkbuff(char *buff, char **line, char **stock)
@@ -29,14 +29,14 @@ static int		ft_checkbuff(char *buff, char **line, char **stock)
 	char	*tmp;
 
 	tmp = ft_strsub(buff, 0, ft_strchr(buff, '\n') - buff);
-   	*line = (*stock == NULL) ? ft_strsub(buff, 0, ft_strchr(buff, '\n') - buff) : 
-   		ft_strjoin(*stock, tmp);
-   	ft_memdel((void **)&tmp);
-   	tmp = (*stock == NULL) ? ft_strjoin(*stock, (ft_strchr(buff, '\n') + 1)) :
-   		ft_strdup(ft_strchr(buff, '\n') + 1);
-   	ft_memdel((void **)stock);
-   	*stock = tmp;
-   	return (0);
+	*line = (*stock == NULL) ? ft_strdup(tmp) :
+		ft_strjoin(*stock, tmp);
+	ft_memdel((void **)&tmp);
+	tmp = (*stock == NULL) ? ft_strjoin(*stock, (ft_strchr(buff, '\n') + 1)) :
+		ft_strdup(ft_strchr(buff, '\n') + 1);
+	ft_memdel((void **)stock);
+	*stock = tmp;
+	return (0);
 }
 
 static int		ft_cutstr(char *buff, char **line, char **stock)
@@ -45,19 +45,32 @@ static int		ft_cutstr(char *buff, char **line, char **stock)
 
 	if (*stock == NULL)
 		*stock = ft_strdup("");
-   	if (ft_iscinstr(*stock, '\n'))
+	if (ft_iscinstr(*stock, '\n'))
 		return (ft_checkstock(buff, line, stock));
 	if (ft_iscinstr(buff, '\n'))
 		return (ft_checkbuff(buff, line, stock));
 	tmp = ft_strjoin(*stock, buff);
 	ft_memdel((void **)stock);
-   	*stock = tmp;
+	*stock = tmp;
 	return (1);
+}
+
+static void		ft_afterread(char **line, char **stock)
+{
+	char	*tmp;
+
+	tmp = ft_strsub(*stock, 0, ft_strchr(*stock, '\n') - *stock);
+	*line = (ft_iscinstr(*stock, '\n')) ? ft_strdup(tmp) :
+		ft_strdup(*stock);
+	ft_memdel((void **)&tmp);
+	tmp = (ft_iscinstr(*stock, '\n')) ? ft_strdup(ft_strchr(*stock, '\n') + 1) :
+		NULL;
+	ft_memdel((void **)stock);
+	*stock = tmp;
 }
 
 int				get_next_line(const int fd, char **line)
 {
-	char			*tmp;
 	static char		*stock;
 	char			buff[BUFF_SIZE + 1];
 	int				ret;
@@ -68,19 +81,13 @@ int				get_next_line(const int fd, char **line)
 	{
 		buff[ret] = '\0';
 		if (ft_cutstr(buff, line, &stock) == 0)
-   			return (1);
+			return (1);
 		ft_bzero(buff, BUFF_SIZE);
 	}
 	if (stock && *stock != '\0')
 	{
-		tmp = ft_strsub(stock, 0, ft_strchr(stock, '\n') - stock); 
-  		*line = (ft_iscinstr(stock, '\n')) ? ft_strdup(tmp) :
-			ft_strdup(stock);
-		ft_memdel((void **)&tmp);
-		tmp = (ft_iscinstr(stock, '\n')) ? ft_strdup(ft_strchr(stock, '\n') + 1) : NULL;
-		ft_memdel((void **)&stock);
-		stock = tmp;
-   		return (1);
+		ft_afterread(line, &stock);
+		return (1);
 	}
 	return (0);
 }
